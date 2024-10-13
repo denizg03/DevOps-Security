4c4e6c53d26d27cdd0835392496dc5d478257eb5
denizg03 <deniz.g03@hotmail.com>
2024-10-13 23:55:04 +0200

    test

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Log hier in!</label>
+<label class="link" for="signinCheckbox">Log hier in!!</label>
 """
 
     return f"""<!DOCTYPE html>
